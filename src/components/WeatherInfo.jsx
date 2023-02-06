31bbe4a2c07d4f00cc2e0070d91da55f517dbe1e
Sxnty <sxntycsgo@gmail.com>
2023-02-06 14:41:50 -0300

    added dynamic info with input

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { WiCloudy } from "react-icons/wi";
 
@@ -6,16 +6,16 @@ function WeatherInfo() {
   const [temp, setTemp] = useState("");
   const [main, setMain] = useState("");
   const [description, setDescription] = useState("");
-  const [cityName, setCityName] = useState("");
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=atlantida&units=metric&appid=5eba1bf17fdacafceb70f666362eb871";
-  const fetchData = async () => {
+  const [cityName, setCityName] = useState("Atlantida");
+  const [newCity, setNewCity] = useState();
+  const fetchData = async (city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5eba1bf17fdacafceb70f666362eb871`;
     const response = await fetch(url);
     const data = await response.json();
     setTemp(data.main.temp);
     setMain(data.weather[0].main);
     setDescription(data.weather[0].description);
-    setCityName(`${data.name}, ${data.sys.country}`)
+    setCityName(`${data.name}, ${data.sys.country}`);
     console.log(data);
   };
 
@@ -26,8 +26,17 @@ function WeatherInfo() {
         break;
     }
   };
+  useEffect(
+    (city) => {
+      fetchData(cityName);
+    },
+    [cityName]
+  );
 
-  fetchData();
+  const changeHandle = (e) => {
+    setNewCity(e.target.value);
+  };
+
   return (
     <main className="hero__weather">
       <h1>{cityName}</h1>
@@ -36,7 +45,14 @@ function WeatherInfo() {
         <h3>{temp}</h3>
         <p>{description}</p>
       </div>
-      <input type="text" placeholder="City name..." />
+      <form
+        onSubmit={(e) => {
+          setCityName(newCity);
+          e.preventDefault()
+        }}
+      >
+        <input type="text" placeholder="City name..." onChange={changeHandle} />
+      </form>
     </main>
   );
 }
