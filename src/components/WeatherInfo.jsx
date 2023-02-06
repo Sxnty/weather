d2a5379f3ad32434c5936ae786659a18364124a9
Sxnty <sxntycsgo@gmail.com>
2023-02-06 15:13:58 -0300

    added grades converter

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { WiCloudy } from "react-icons/wi";
-
 function WeatherInfo() {
   const [temp, setTemp] = useState("");
   const [main, setMain] = useState("");
   const [description, setDescription] = useState("");
   const [cityName, setCityName] = useState("Atlantida");
   const [newCity, setNewCity] = useState();
-  const fetchData = async (city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=5eba1bf17fdacafceb70f666362eb871`;
+  const [grades, setGrades] = useState('metric');
+  const fetchData = async (city, grades) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${grades}&appid=5eba1bf17fdacafceb70f666362eb871`;
     const response = await fetch(url);
     const data = await response.json();
     setTemp(data.main.temp);
@@ -17,7 +17,15 @@ function WeatherInfo() {
     setDescription(data.weather[0].description);
     setCityName(`${data.name}, ${data.sys.country}`);
     console.log(data);
+    
   };
+  const info = {
+    temp: temp,
+    main: main,
+    description: description,
+    cityName: cityName,
+    grades: grades
+  }
 
   const dataToIcon = () => {
     switch (main) {
@@ -28,31 +36,48 @@ function WeatherInfo() {
   };
   useEffect(
     (city) => {
-      fetchData(cityName);
+      fetchData(info.cityName, grades);
     },
-    [cityName]
+    [info.cityName, info.grades]
   );
 
   const changeHandle = (e) => {
     setNewCity(e.target.value);
   };
+const gradesConverter = () => {
+    if(info.grades === 'metric') {
+        return 'Celcius'
+      } else {
+        return 'Farenheit'
+      }
+}
 
   return (
     <main className="hero__weather">
-      <h1>{cityName}</h1>
+      <h1>{info.cityName}</h1>
       {dataToIcon()}
       <div className="weather__info">
-        <h3>{temp}</h3>
-        <p>{description}</p>
+        <h3>{info.temp}</h3>
+        <p>{info.description}</p>
       </div>
       <form
         onSubmit={(e) => {
           setCityName(newCity);
-          e.preventDefault()
+          e.preventDefault();
         }}
       >
         <input type="text" placeholder="City name..." onChange={changeHandle} />
       </form>
+      
+      <button onClick={(e) => {
+        e.preventDefault()
+        if(info.grades === 'metric') {
+            setGrades('imperial')
+        } else {
+            setGrades('metric')
+        }
+
+      }}>{gradesConverter()}</button>
     </main>
   );
 }
