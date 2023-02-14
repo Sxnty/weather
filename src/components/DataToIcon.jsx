69f6b5e1d6c01a76fe4959f0fd462328931678bc
Sxnty <sxntycsgo@gmail.com>
2023-02-14 18:36:38 -0300

    added mobile responsive

--- src/components/DataToIcon.jsx
+++ src/components/DataToIcon.jsx
@@ -1,36 +1,20 @@
 import React from "react";
 import { WiCloudy, WiDaySunny, WiDayHaze, WiShowers } from "react-icons/wi";
-    const DataToIcon = ({main}) => {
-        switch (main) {
-          case "Clear":
-            return (
-              <i>
-                <WiDaySunny />
-              </i>
-            );
-            break;
-          case 'Clouds':
-          return (
-            <i>
-            <WiCloudy />
-          </i>
-          )
-          break;
-          case 'Haze':
-            return(
-              <i>
-              <WiDayHaze />
-            </i>
-            )
-            break;
-            case 'Rain':
-              return(
-                <i>
-                <WiShowers />
-              </i>
-              )
-              break;
-        }
-      };
+const DataToIcon = ({ main }) => {
+  switch (main) {
+    case "Clear":
+      return <WiDaySunny />;
+      break;
+    case "Clouds":
+      return <WiCloudy />;
+      break;
+    case "Haze":
+      return <WiDayHaze />;
+      break;
+    case "Rain":
+      return <WiShowers />;
+      break;
+  }
+};
 
-export default DataToIcon
+export default DataToIcon;
