78e3498abd309daeba2455fac7ceaf4084c2f741
Sxnty <sxntycsgo@gmail.com>
2023-02-24 20:44:32 -0300

    added momentjs and changed the forecast date info

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useContext, useState, useEffect } from "react";
 import { CityContext } from "../context/CityContext";
 import DataToIcon from "./DataToIcon";
+import moment from "moment";
 import ForecastCard from "./ForecastCard";
 function Forecast() {
   const { info, list, setList, setGrades } = useContext(CityContext);
@@ -18,7 +19,9 @@ function Forecast() {
             <DataToIcon main={element.weather[0].main} />
             <h2>{element.main.temp}</h2>
             <p>{element.weather[0].description}</p>
-            <span>{element.dt_txt}</span>
+            <span>{moment(element.dt_txt).format('dddd')}</span>
+            <span>{moment(element.dt_txt).format('HH:mm')}</span>
+
           </div>
         );
       })}
